9b373894403830bd560cc5f8d3e1a19d9513062d
erik-rik <erik.knabach@gmail.com>
2026-04-11 13:17:58 -0300

    Create Livro.java
    Criando o model de livro

---
+++ biblioteca/src/main/java/br/com/biblioteca/model/Livro.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.biblioteca.model;
+
+/**
+ *
+ * @author erikk
+ */
+import jakarta.persistence.*;
+
+@Entity
+public class Livro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String titulo;
+    private String autor;
+    private boolean disponivel;
+
+    public Livro() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+}
