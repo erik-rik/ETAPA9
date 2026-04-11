8417234099caf2a6191369072654f94ffa84358f
erik-rik <erik.knabach@gmail.com>
2026-04-11 13:17:16 -0300

    add usuario model
    criando o model de usuario

---
+++ biblioteca/src/main/java/br/com/biblioteca/model/Usuario.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.biblioteca.model;
+
+/**
+ *
+ * @author erikk
+ */
+import jakarta.persistence.*;
+
+@Entity
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String nome;
+    private String email;
+    private String senha;
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
+
