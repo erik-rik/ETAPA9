650529fd159f8e2b47eb2df6c14cfb150bfe5d00
erik-rik <erik.knabach@gmail.com>
2026-04-11 15:12:57 -0300

    saving project

--- biblioteca/src/main/resources/static.js/script.js
+++ biblioteca/src/main/resources/static.js/script.js
@@ -4,3 +4,45 @@
  */
 
 
+function validarLogin() {
+    let email = document.getElementById("email").value;
+    let senha = document.getElementById("senha").value;
+
+    if (email === "" || senha === "") {
+        alert("Preencha todos os campos!");
+        return false;
+    }
+    return true;
+}
+
+function validarUsuario() {
+    let nome = document.querySelector("input[name='nome']").value;
+    let email = document.querySelector("input[name='email']").value;
+
+    if (nome === "" || email === "") {
+        alert("Preencha todos os campos!");
+        return false;
+    }
+    return true;
+}
+
+function validarLivro() {
+    let titulo = document.querySelector("input[name='titulo']").value;
+
+    if (titulo === "") {
+        alert("Informe o título!");
+        return false;
+    }
+    return true;
+}
+
+function validarEmprestimo() {
+    let usuario = document.querySelector("input[name='usuario.id']").value;
+    let livro = document.querySelector("input[name='livro.id']").value;
+
+    if (usuario === "" || livro === "") {
+        alert("Preencha usuário e livro!");
+        return false;
+    }
+    return true;
+}
